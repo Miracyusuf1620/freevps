17a6605ee3bfb67486ce527601b89e69a33b0d49
Miracyusuf1620 <124456823+Miracyusuf1620@users.noreply.github.com>
2024-01-04 20:34:56 +0300

    Delete main.ps1

--- main.ps1
+++
@@ -1,2 +0,0 @@
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
